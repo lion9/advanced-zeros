09210d30786960674f2a83a763481e069b4a68e2
Artyom Vecherov <a.vecherov@gmail.com>
2019-02-25 17:57:58 +0300

    trailing zeroes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var noz = Number.MAX_VALUE;
+
+  var j = base;
+  for (var i = 2; i <= base; i++) {
+    if (j % i === 0) {
+      var p = 0;
+      while (j % i === 0) {
+         j = j / i;     
+         p++;
+      }
+      var c = 0;
+      var z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      noz = Math.min(noz, Math.floor(c / p))
+    }
+  }
+  return noz;
 }
